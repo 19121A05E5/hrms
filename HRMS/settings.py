163ae861c5e6b25b58444c79084cd32f6f800d9d
19121A05E5 <86068887+19121A05E5@users.noreply.github.com>
2023-09-12 18:24:54 +0530

    Update settings.py

--- HRMS/settings.py
+++ HRMS/settings.py
@@ -102,8 +102,8 @@ DATABASES = {
         'NAME': 'hrms2',
         'USER': 'root',
         'PASSWORD': '',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'HOST': 'db',
+        #'PORT': '3306',
        # 'OPTIONS': {'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"},
     }
 }
